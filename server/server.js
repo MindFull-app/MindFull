6423d56a6e8a448f1ceb557bc899159d0b8102df
Edward Cho <edwardcho1231@gmail.com>
2021-01-12 11:06:13 -0500

    added user router, to handle create user

--- server/server.js
+++ server/server.js
@@ -1,16 +1,18 @@
 const express = require('express');
-const path = require('path');
+require('dotenv').config();
+
+const userRouter = require('./routes/user')
+
+const { instance } = require('../db/db');
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
 
-
-
-
+// user router
+app.use('/user', userRouter)
 
 // add 404 status code in case url cannot be found at server
 app.use('*', (req, res) => res.sendStatus(404));
@@ -19,12 +21,18 @@ app.use('*', (req, res) => res.sendStatus(404));
 app.use((err, req, res, next) => {
   err.statusCode = err.statusCode || 400;
   err.message = err.message || 'error';
+  // internal logging for debugging
+  console.log(err.log);
 
   res.status(err.statusCode).json({
     message: err.message,
   });
 });
 
-app.listen(PORT, () => {
-  console.log(`Listening to port ${PORT}`);
+// add {force: true} in the argument for instance.sync
+// to drop tables and create new ones at server initiation
+instance.sync({force: true}).then(() => {
+  app.listen(PORT, () => {
+    console.log(`Listening to port ${PORT}`);
+  });
 });
