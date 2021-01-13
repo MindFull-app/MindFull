125c66ed78633129ac3d1df8b1e1e8bf0b8e0ab8
Edward Cho <edwardcho1231@gmail.com>
2021-01-12 21:56:11 -0500

    create signup page and update styles

--- client/components/SignUp.jsx
+++ client/components/SignUp.jsx
@@ -1,16 +1,14 @@
 import React from "react"
-import ReactDOM from 'react-dom';
+import { Link } from 'react-router-dom';
 import {Button, TextField, MenuItem} from '@material-ui/core';
 
-import Header from './components/Header.jsx';
+import SimpleCard from './views/SimpleCard.jsx'
+import SimpleForm from './views/SimpleForm.jsx'
 
 function SignUp() {
   return (
-    <div>
-      <Header />
-      <br/>
-      {/* first_name, last_name, email, password, patient, therapist, */}
-      <form className="class" noValidate autoComplete="off">
+    <SimpleCard >
+      <SimpleForm btn='SignUp'>
         <TextField required id="signup_username" label="Email" />
         <br/>
         <TextField required id="signup_password" label="Password" />
@@ -23,11 +21,12 @@ function SignUp() {
           <MenuItem>Therapist</MenuItem>
           <MenuItem>Patient</MenuItem>
         </TextField>
-      </form>
-      <Button variant="contained" color="primary">
-        Enter
-      </Button>
-    </div>
+      </SimpleForm>
+      <div>
+        Already a member?
+        <Link to='/'><Button>LogIn</Button></Link>
+      </div>
+    </SimpleCard>
   );
 }
 
