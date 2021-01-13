6892d4b5b3c7eaeafb3166bf5290377cd92a28e8
Edward Cho <edwardcho1231@gmail.com>
2021-01-13 11:37:59 -0500

    create state to keep user info in App component

--- client/App.jsx
+++ client/App.jsx
@@ -1,17 +1,20 @@
 import React from "react"
 import { Route, Switch } from 'react-router-dom';
+import {useState} from 'react';
 
 import Header from './components/Header.jsx';
 import Login from './components/Login.jsx';
 import SignUp from './components/SignUp.jsx';
 
 function App() {
+  const [user, setUser] = useState(null);
+  console.log(user);
   return (
     <main>
       <Header />
       <Switch>
         <Route path='/' component={Login} exact />
-        <Route path='/signup' component={SignUp} />
+        <Route path='/signup' render={(props) => <SignUp {...props} setUser={setUser} />} />
       </Switch>
     </main>
   );
